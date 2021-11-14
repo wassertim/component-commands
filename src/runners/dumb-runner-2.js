af416dd51d057d5b101a2fdd6d4bbe1f3f30e858
wassertim <tim.tkachenko@idealo.de>
2021-11-14 17:08:19 +0100

    add missing files

---
+++ src/runners/dumb-runner-2.js
@@ -0,0 +1,38 @@
+const swap = (array, x, y) => ([array[x], array[y]] = [array[y], array[x]]);
+
+export let runState = {
+  isOnPause: false,
+};
+
+async function delay(timeout) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
+
+async function wait() {
+  while (runState.isOnPause) {
+    await delay(10);
+  }
+
+  return Promise.resolve();
+}
+
+export async function run(elements, setAction) {
+  const steps = [];
+  let m = elements.length;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < m - i; j++) {
+      steps.push({ type: "changeSwapIndex", swapIndex: j });
+      setAction({ type: "changeSwapIndex", swapIndex: j });
+      await delay(1000);
+      await wait();
+      if (elements[j] > elements[j + 1]) {
+        swap(elements, j, j + 1);
+        setAction({ type: "swap" });
+        await delay(1000);
+      }
+    }
+  }
+  return steps;
+}
