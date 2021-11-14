344f5ff94d320dff84f0f15ec824ec2316b06055
wassertim <tim.tkachenko@idealo.de>
2021-11-14 11:48:40 +0100

    wip

--- src/item-list/item-list.js
+++ src/item-list/item-list.js
@@ -1,12 +1,19 @@
 import "./item-list.scss";
 import { Item } from "./item/item";
 
-export function ItemList({ items, swapIndex }) {  
+export function ItemList({ items, swapIndex }) {
   return (
     <div className="item-list group">
-      {items.map((item, i) => {
-        return <Item key={i} item={item} highlighted={[swapIndex, swapIndex+1].includes(item.currentPosition)} height={item.height} />;
-      })}
+      {items.map((item, i) => (
+        <Item
+          key={i}
+          item={item}
+          highlighted={[swapIndex, swapIndex + 1].includes(
+            item.currentPosition
+          )}
+          height={item.height}
+        />
+      ))}
     </div>
   );
 }
