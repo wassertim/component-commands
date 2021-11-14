a1869198d675ed957a937f97b6c1c4fad25596a2
wassertim <tim.tkachenko@idealo.de>
2021-11-14 10:20:46 +0100

    add margins

--- src/item-list/item/item.js
+++ src/item-list/item/item.js
@@ -2,9 +2,13 @@ import "./item.css";
 import React from "react";
 
 export const Item = React.forwardRef(({ height, item }, ref) => {
+  const width = 20;
+  const margin = 1;
   const style = {
     height: `${height}%`,
-    transform: `translate(${(item.currentPosition - item.originalPosition) * 22}px)`
+    width: `${width}px`,
+    margin: `${margin}px`,
+    transform: `translate(${(item.currentPosition - item.originalPosition) * (width + margin*2)}px)`,
   };
   return (
     <div ref={ref} className="item-list__item box" style={style}>
